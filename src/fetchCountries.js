47aec93814e77c7c8fc09bf1adbc9476dd15132d
DidorchukAlexandr <104354949+DidorchukAlexandr@users.noreply.github.com>
2022-10-19 21:39:46 +0300

    fixed

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,8 @@
 const BASE_URL = 'https://restcountries.com/v3.1/name/';
 
-export const fetchCountries = name => {
+export const fetchCountries = data => {
     return fetch(
-        `${BASE_URL}${name}?fields=name,capital,population,flags,languages`
+        `${BASE_URL}${data}?fields=name,capital,population,flags,languages`
     ).then(response => {
         if (!response.ok) {
             throw new Error(response.status);
